426c005ec42b666f5ea85bb39b526a51711a059e
Changdong Deng <dengchangdong@gmail.com>
2025-07-20 17:07:12 +0800

    Upload Files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BLOG_TITLE: process.env.BLOG_TITLE,
+    BLOG_DESCRIPTION: process.env.BLOG_DESCRIPTION,
+    AUTHOR_NAME: process.env.AUTHOR_NAME,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN
+  }
+};
 
 export default nextConfig;
