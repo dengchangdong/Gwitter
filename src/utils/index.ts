5ffe64ce8731fbde3e249de9350d1297ec20ac21
Changdong Deng <dengchangdong@gmail.com>
2025-07-20 21:56:07 +0800

    Upload Files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -190,8 +190,8 @@ export const getColorByBgColor = (hexcolor: string) => {
   let r = parseInt(hexcolor.substr(0, 2), 16);
   let g = parseInt(hexcolor.substr(2, 2), 16);
   let b = parseInt(hexcolor.substr(4, 2), 16);
-  let yiq = (r * 299 + g * 587 + b * 114) / 1000;
-  return yiq >= 128 ? 'black' : 'white';
+  let brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness >= 128 ? 'black' : 'white';
 };
 
 export const queryStringify = (query: Record<string, string>) => {
